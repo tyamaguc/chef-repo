2924ea498aec452058a888f4d21039d19c8411b2
tomoyuki-yamaguchi <tomoyuki-yamaguchi@gree.net>
2013-03-29 21:05:12 +0900

    locale の　cookbook を修正

--- site-cookbooks/locale/attributes/default.rb
+++ site-cookbooks/locale/attributes/default.rb
@@ -1,1 +1,2 @@
-default[:locale][:lang] = "ja_JP.UTF-8"
+default[:locale][:lang]   = "ja_JP.UTF-8"
+default[:locale][:encode] = "ja_JP.UTF-8 UTF-8"
--- site-cookbooks/locale/recipes/default.rb
+++ site-cookbooks/locale/recipes/default.rb
@@ -8,6 +8,17 @@
 #
 package "locales"
 
+bash "append_locale" do
+  user "root"
+  code <<-EOH
+  echo #{node[:locale][:encode]} >> /etc/locale.gen
+  EOH
+end
+ 
+execute "locale_gen" do
+  command "locale-gen"
+end
+
 execute "update locale" do
   command "update-locale LANG=#{node[:locale][:lang]}"
 end
